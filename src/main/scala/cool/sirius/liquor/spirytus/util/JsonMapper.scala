7bee91e07213a3c1e07335da32f2d2245368a68e
Sirius <i@sirius.cool>
2020-08-14 17:44:24 +0800

    add JsonMapper.

--- src/main/scala/cool/sirius/liquor/spirytus/util/JsonMapper.scala
+++ src/main/scala/cool/sirius/liquor/spirytus/util/JsonMapper.scala
@@ -1,7 +1,14 @@
 package cool.sirius.liquor.spirytus.util
 
+import net.liftweb.json.Serialization.{read, write}
+import net.liftweb.json.{FieldSerializer, Formats, Serialization, ShortTypeHints}
+
 object JsonMapper {
 
+  implicit val formats: Formats = Serialization.formats(ShortTypeHints(List())) + FieldSerializer[String]()
 
+  def from[T](jsonString: String)(implicit mf: Manifest[T]): T = read[T](jsonString)
+
+  def to(in: Any): String = write(in)
 
 }
