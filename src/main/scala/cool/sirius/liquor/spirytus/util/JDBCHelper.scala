0010cce48004e4bcaf261cdbe4c2a81c7f850cc6
Sirius <i@sirius.cool>
2020-09-22 17:45:51 +0800

    add two more helper function in JDBCHelper.

--- src/main/scala/cool/sirius/liquor/spirytus/util/JDBCHelper.scala
+++ src/main/scala/cool/sirius/liquor/spirytus/util/JDBCHelper.scala
@@ -31,6 +31,35 @@ trait JDBCHelper extends JavaCollectionMapper {
       objectMapper.convertValue(jdbcTemplate.queryForMap(sql).toScala.map{case (k, v) => (CaseUtils.toCamelCase(k, false), v)}, clazz)
     }
 
+    def queryForMapIgnoreType(): List[Map[String, String]] = {
+      jdbcTemplate.queryForList(sql).toScala.map(entry => {
+        entry.toScala.map{ case (k, v) => (k, Option(v).fold("")(_.toString))}
+      })
+    }
+
+    def queryForMap(): List[Map[String, Any]] = {
+      jdbcTemplate.queryForList(sql).toScala.map(entry => {
+        entry.toScala.map{case (k, v) => {
+          v match {
+            case i@Int =>
+              (k, i)
+
+            case d@Double =>
+              (k, d)
+
+            case f@Float =>
+              (k, f)
+
+            case l@Long =>
+              (k, l)
+
+            case _ =>
+              (k, v.toString)
+          }
+        }}
+      })
+    }
+
     def execute(): Unit = {
       jdbcTemplate.execute(sql)
     }
