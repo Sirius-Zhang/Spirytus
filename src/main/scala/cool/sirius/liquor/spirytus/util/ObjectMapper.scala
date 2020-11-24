a8be8632f6c86cfc24f16ace1246086d5bc11469
Sirius <i@sirius.cool>
2020-11-24 17:52:01 +0800

    enhanced cc2map

--- src/main/scala/cool/sirius/liquor/spirytus/util/ObjectMapper.scala
+++ src/main/scala/cool/sirius/liquor/spirytus/util/ObjectMapper.scala
@@ -6,7 +6,19 @@ trait ObjectMapper {
 
   implicit class CC2Map(in: Product) {
     def toMap: Map[String, Any] = {
-      (in.productElementNames zip in.productIterator).toMap
+      (in.productElementNames zip in.productIterator).map{
+        case (k: String, null) =>
+          (k, "Null")
+
+        case (k: String, v: Option[Product]) =>
+          v.fold[(String, Any)]((k,None))(z => (k, z.toMap))
+
+        case (k: String, v: Product) =>
+          (k, v.toMap)
+
+        case (k: String, v: Any) =>
+          (k, v)
+      }.toMap
     }
   }
 
