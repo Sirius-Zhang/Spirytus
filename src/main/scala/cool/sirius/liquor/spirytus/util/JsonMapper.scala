f87e3160b50aa5869d7ff1f5c98dcf1d9c17aaa3
Sirius <i@sirius.cool>
2021-01-06 15:20:24 +0800

    change implicit format for jsonmapper.

--- src/main/scala/cool/sirius/liquor/spirytus/util/JsonMapper.scala
+++ src/main/scala/cool/sirius/liquor/spirytus/util/JsonMapper.scala
@@ -1,11 +1,11 @@
 package cool.sirius.liquor.spirytus.util
 
 import net.liftweb.json.Serialization.{read, write}
-import net.liftweb.json.{FieldSerializer, Formats, Serialization, ShortTypeHints}
+import net.liftweb.json.{DefaultFormats, Formats}
 
 object JsonMapper {
 
-  implicit val formats: Formats = Serialization.formats(ShortTypeHints(List())) + FieldSerializer[String]()
+  implicit val formats: Formats = DefaultFormats
 
   def from[T](jsonString: String)(implicit mf: Manifest[T]): T = read[T](jsonString)
 
